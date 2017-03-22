7a0a962c7f6339188318b2bef09f2c0fb8fdf9cf
Aman Dwivedi <dwivedi.aman96@gmail.com>
2017-03-22 20:58:11 +0530

    latest code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
     println!("Hello, world!");
+    let x = 5 + /* 40 + */ 5;
+    println!("Is x 10 or 50? x = {}", x);
+    println!("{} days", 31);
+    println!("{0} is {1}'s friend!","Aman", "Sachin");
+    println!("{a} is {b} {c} works!", a = "This", b = "how", c = "Rust");
+    println!("{:b} is binary of {}", 2, 2);
 }
