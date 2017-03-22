a87c03b74d9a81ffd153e991b0b5b1f5d3c9d152
Aman Dwivedi <dwivedi.aman96@gmail.com>
2017-03-23 00:32:40 +0530

    latest code

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,22 @@ impl fmt::Display for Complex {
     }
 }
 
+struct List(Vec<i32>);
+
+impl fmt::Display for List {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let vec = &self.0;
+        write!(f, "[")?;
+        for (count, v) in vec.iter().enumerate() {
+            // For every element except the first, add a comma.
+            // Use the ? operator, or try!, to return on errors.
+            if count != 0 { write!(f, ", ")?; }
+            write!(f, "{}", v)?;
+        }
+        write!(f, "]")
+    }
+}
+
 fn main() {
     println!("Hello, world!");
     let x = 5 + /* 40 + */ 5;
@@ -35,4 +51,7 @@ fn main() {
     let complex = Complex{real : 3.3, imag : 7.2};
     println!("Display: {}", complex);
     println!("Debug: {:?}", complex);
+
+    let v = List(vec![1, 2, 3]);
+    println!("{}", v);
 }
